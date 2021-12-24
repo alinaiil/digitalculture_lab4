f343007910df23e039c27f0a970f42bf8c6a1cf7
alinaiil <alinail03@mail.ru>
2021-12-24 03:45:02 +0300

    error fixed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def test():
     assert (main(1, 1, '-') == 0)
     assert (main(-1, 1, '+') == 0)
-    assert (main(2, 4, '*') == 14)
+    assert (main(2, 7, '*') == 14)
     assert (main(8, 2, '/') == 4)
     print('All tests passed')
 
